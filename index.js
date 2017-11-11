8f3ed6cf707726dabe5ab3a7405ee14b1ae939fe
Scott Ballantyne <ussballantyne@gmail.com>
2017-11-11 16:53:48 -0600

    Improving the readme and also make changes according to the slightly different api that node paperclip is now using

--- index.js
+++ index.js
@@ -8,15 +8,25 @@ module.exports    = klass(function(paperclip) {
 
   perform: function(options, next) {
     var self      = this.paperclip;
-
+    var attribute;
+    if (options.attribute) {
+      attribute   = options.attribute;
+    } else {
+      attribute   = 'qrcode';
+    }
     Jimp.read(self.file.buffer, function(err, image) {
       if (err) {
         console.error(err);
       }
       var qr = new QrCode();
       qr.callback = function(err, value) {
+        var object = {};
+        if (result) {
+          object[attribute] = value;
+        }
+
         if (next) {
-          next(err, {qrcode: value});
+          next(err, object);
         }     
       };
       qr.decode(image.bitmap);
